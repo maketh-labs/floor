ee8fbb06b7b18a46094237fbc26ee177d54b8cb2
Ion Jo <monsieur.mansa.musa@gmail.com>
2026-06-23 22:31:29 +0900

    feat: remove value == 0 check to enable free games

--- src/CommitReveal.sol
+++ src/CommitReveal.sol
@@ -206,7 +206,6 @@ contract CommitReveal is
         nonReentrant
     {
         if (block.timestamp > params.deadline) revert SignatureExpired();
-        if (params.betAmount == 0) revert InvalidAmount(params.betAmount);
 
         // Calculate game ID from signature hash
         bytes32 gameId = keccak256(serverSignature);
@@ -245,7 +244,6 @@ contract CommitReveal is
         bytes calldata permitSignature
     ) external nonReentrant {
         if (block.timestamp > params.deadline) revert SignatureExpired();
-        if (params.betAmount == 0) revert InvalidAmount(params.betAmount);
         if (params.token == ETH_ADDRESS) revert InvalidAsset();
 
         // Calculate game ID from signature hash
